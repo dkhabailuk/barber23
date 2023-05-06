2e4471297a893c6cc489a282f47a6e383e1105c3
dkhabailuk <95091427+dkhabailuk@users.noreply.github.com>
2023-05-06 09:50:43 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('button').onclick = function() {
+    document.getElementById('list').classList.toggle('active');
+    document.getElementById('button').classList.toggle('active');
+    document.getElementById('img').classList.toggle('active');
+}
